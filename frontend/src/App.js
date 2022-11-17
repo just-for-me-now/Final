3857223f47b555a815ca5dbeefe4a36b9ebddc73
David Paradela <david.paradela@solera.com>
2022-11-17 15:05:51 +0100

    fix useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Container from './components/Container';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 function App() {
 
   const[data, setData] = useState();
 
-  fetch('http://localhost:8080/employees')
+  useEffect(()=>{
+    fetch('http://localhost:8080/employees')
     .then(response => response.json())
-    .then(response => console.log(response))
     .then(response => setData(response));
-
+  }, []);
 
   return (
     <div className="App">
--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -2,6 +2,11 @@ import Card from "./Card";
 import Empty from "./Empty";
 import '../App.css';
 function Container(props) {
+
+  const dataHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="cardsContainer">
       {props.data?.map(employee => <Card employee={employee}/>)}
