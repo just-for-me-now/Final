3810d4ee2b92cfa466012ee935c389b20b4399be
David Paradela <david.paradela@solera.com>
2022-11-17 10:37:39 +0100

    fix employee json properties

--- backend/src/main/java/com/example/demo/model/Employee.java
+++ backend/src/main/java/com/example/demo/model/Employee.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.NotFound;
 
 @Data
 @Entity
@@ -12,12 +16,20 @@ public class Employee {
     @GeneratedValue
     private long id;
 
-    private String firtname;
+    @JsonProperty("first-name")
+    @NotNull
+    private String firstName;
 
+    @JsonProperty("last-name")
+    @NotNull
     private String lastName;
 
+    @JsonProperty("phone-number")
+    @NotNull
     private String phoneNumber;
 
+    @JsonProperty("email")
+    @NotNull
     private String email;
 
     public long getId() {
