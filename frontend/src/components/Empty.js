93359fca9a8609fcc79b9ca08d252a4d88c7a239
Javier Padin <javier.padin@solera.com>
2022-11-17 15:51:20 +0100

    added 'props' somewhere

--- frontend/src/components/Empty.js
+++ frontend/src/components/Empty.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import '../App.css';
-function Empty() {
+function Empty(props) {
 
     const [firstName, setName] = useState("");
     const [lastName, setLastName] = useState("");
